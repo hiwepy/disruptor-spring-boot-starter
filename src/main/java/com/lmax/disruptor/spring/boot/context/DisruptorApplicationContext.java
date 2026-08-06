fc69f7408c2d8befd42ccd1326057048112d5d2e
wandl-6A72h <hiwepy@gmail.com>
2026-08-07 01:53:32 +0800

    docs: 新增中英文双语 README（统一格式）

--- src/main/java/com/lmax/disruptor/spring/boot/context/DisruptorApplicationContext.java
+++ src/main/java/com/lmax/disruptor/spring/boot/context/DisruptorApplicationContext.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2017, hiwepy (https://github.com/hiwepy).
+ * Copyright (c) 2017, hiwepy (https://github.com/easy-4-java).
  *
  * Licensed under the Apache License, Version 2.0 (the "License"); you may not
  * use this file except in compliance with the License. You may obtain a copy of
